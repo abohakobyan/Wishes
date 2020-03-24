ef6aee2e970db11441a86ab4d834624a1ffdb36c
aboha <aboha@Excalibur>
2020-03-24 17:48:32 -0400

    Removed

--- LogingSecure/src/main/java/com/abo/demo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abo.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<users, Long> {
-	users findByUsername(String username);
-}
